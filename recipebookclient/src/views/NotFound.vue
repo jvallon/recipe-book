c9454e852bc32adda7722157cd71c20891f27802
Josh Vallon <joshua.vallon@gmail.com>
2021-05-26 14:20:13 -0700

    feat: add 404 page

---
+++ recipebookclient/src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <center>
+      <h1>Not Found</h1>
+      <v-btn @click="$router.back()">Go back</v-btn>
+    </center>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+center {
+  margin: 15vw;
+}
+
+</style>
