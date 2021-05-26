3f6bb67dc28701fd9ef82fb0e098d51a598bb820
Josh Vallon <joshua.vallon@gmail.com>
2021-05-26 13:24:32 -0700

    feat: add action

--- recipebookclient/src/components/RecipeCard.vue
+++ recipebookclient/src/components/RecipeCard.vue
@@ -6,11 +6,14 @@
       :src="src"
       class="white--text align-end"
       gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-      height="200px">
+      height="250px">
       <v-card-title>{{ title }}</v-card-title>
     </v-img>
     <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
     <v-card-text>{{ description }}</v-card-text>
+    <v-card-actions>
+      <v-btn>Try it!</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
